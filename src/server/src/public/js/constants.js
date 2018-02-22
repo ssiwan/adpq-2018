7d7f3e6841dab9e8bee13d17fbe9e997befc2112
HOTB\raghu.jonnala <rjonnala@counselordirect.com>
2018-02-22 14:57:56 -0800

    RJ - create article design and bug fixes

---
+++ src/server/src/public/js/constants.js
@@ -0,0 +1,2 @@
+var APIURL = "http://adpq-staging.hotbsoftware.com/api/v1/";
+//var APIURL = "http://localhost:3001/api/v1/";
---
+++ src/server/src/public/js/quill-custom.js
@@ -0,0 +1,21 @@
+
+        var toolbarOptions = [['bold', 'italic'], ];
+        var toolbarOptions = [
+          ['bold', 'italic', 'underline', 'strike'],        
+          ['blockquote', 'code-block'],
+          ['link', 'image'],
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          [{ 'script': 'sub'}, { 'script': 'super' }],      
+          [{ 'indent': '-1'}, { 'indent': '+1' }],          
+          [{ 'direction': 'rtl' }],                         
+          [{ 'color': [] }, { 'background': [] }],          
+          [{ 'font': [] }],
+          [{ 'align': [] }],
+          ['clean']                                         
+        ];
+        var quill = new Quill('#editor', {
+          modules: {
+            toolbar: toolbarOptions
+          },
+          theme: 'snow'
+        });
