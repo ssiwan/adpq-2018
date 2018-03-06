d55d9edd853a9ecf6d3f2f05b76b8248b11e3b63
Eric Dobyns <edobyns@hotbsoftware.com>
2018-03-06 14:27:30 -0800

    Updated qa scripts

--- src/devops/scripts/deploy.groovy
+++ src/devops/scripts/deploy.groovy
@@ -62,27 +62,26 @@ def runStagingTests() {
     stage('Test') {
         sh '''#!/bin/bash
             # Wait 10 seconds
-            sleep 10
+            sleep 10 &&
 
             # Clean Up
-            rm -rf /var/lib/jenkins/adpq_test_results
-            mkdir /var/lib/jenkins/adpq_test_results
+            rm -rf /var/lib/jenkins/adpq_test_results &&
+            mkdir /var/lib/jenkins/adpq_test_results &&
 
-
-
+            # Build and run api & db
             /usr/local/bin/docker-compose up --build -d &&
 
             # Build & run container
             docker build ./src/qa -t adpq_tests &&
             docker run -v /var/lib/jenkins/adpq_test_results/reports:/data/reports -e Environment=local --name adpq_tests -i adpq_tests >> /var/lib/jenkins/adpq_test_results/results.xml &&
-            docker rm adpq_tests && docker rmi adpq_tests
+            docker rm adpq_tests && docker rmi adpq_tests &&
 
             # Extract test results and save to var RESULTS
-            numberOfTests=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 2 | cut -d ' ' -f 1)
-            errors=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 3 | cut -d ' ' -f 1)
-            failures=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 4 | cut -d '>' -f 1)
-            numberOfSuccesses=$(($numberOfTests - $errors - $failures))
-            echo "$numberOfSuccesses / $numberOfTests tests ran successfully" > RESULTS
+            numberOfTests=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 2 | cut -d ' ' -f 1) &&
+            errors=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 3 | cut -d ' ' -f 1) &&
+            failures=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 4 | cut -d '>' -f 1) &&
+            numberOfSuccesses=$(($numberOfTests - $errors - $failures)) &&
+            echo "$numberOfSuccesses / $numberOfTests tests ran successfully" > RESULTS &&
 
             # Create Shield.io badge url & set Slack result type
             testBadge=""
@@ -98,6 +97,8 @@ def runStagingTests() {
             curl $testBadge >> ./testResultsImg.svg
             aws s3 cp --acl public-read ./testResultsImg.svg s3://adpq-assets/buildAssets/testResults.svg
             rm -rf ./testResultsImg.svg
+
+            docker stop api && docker rm api && docker rmi api && docker stop db && docker rm db
         '''
     }
 }
