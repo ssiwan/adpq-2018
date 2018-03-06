e88d76969bcb8944ac0ef475e01c7792f7a5bcb7
Eric Dobyns <edobyns@hotbsoftware.com>
2018-03-06 14:08:43 -0800

    Added Noel's changes; Updated qa test deployment scripts

--- src/devops/scripts/deploy.groovy
+++ src/devops/scripts/deploy.groovy
@@ -76,7 +76,6 @@ def runStagingTests() {
             docker build ./src/qa -t adpq_tests &&
             docker run -v /var/lib/jenkins/adpq_test_results/reports:/data/reports -e Environment=local --name adpq_tests -i adpq_tests >> /var/lib/jenkins/adpq_test_results/results.xml &&
             docker rm adpq_tests && docker rmi adpq_tests
-            docker rm api && docker rmi api
 
             # Extract test results and save to var RESULTS
             numberOfTests=$(cat /var/lib/jenkins/adpq_test_results/results.xml | cut -d '=' -f 2 | cut -d ' ' -f 1)
