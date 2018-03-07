274982d9216fd4940986a7afeabb20c712923acf
Eric Dobyns <edobyns@hotbsoftware.com>
2018-03-06 17:41:53 -0800

    Updated docker commands

--- src/devops/scripts/deploy.groovy
+++ src/devops/scripts/deploy.groovy
@@ -103,9 +103,9 @@ def runStagingTests() {
             aws s3 cp --acl public-read ./testResultsImg.svg s3://adpq-assets/buildAssets/testResults.svg
             rm -rf ./testResultsImg.svg
 
-            docker volume rm $(docker volume ls -qf dangling=true)
-            docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-            docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+            docker kill $(docker ps -q) || true
+            docker rm $(docker ps -a -q) || true
+            docker rmi $(docker images -q) || true
         '''
     }
 }
