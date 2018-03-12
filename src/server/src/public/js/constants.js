2795bc61e56143e3b0e7e7b0c6e435922a2bfab9
Eric Dobyns <edobyns@hotbsoftware.com>
2018-03-12 10:32:19 -0700

    Reverted constants.js

--- src/server/src/public/js/constants.js
+++ src/server/src/public/js/constants.js
@@ -1,23 +1,21 @@
 'use strict'
 
-var APIURL;
-var SocialMediaURL;
+var APIURL = "https://adpq-staging.hotbsoftware.com/api/v1/";
+var SocialMediaURL = "https://adpq-staging.hotbsoftware.com/";;
 
-switch (process.env.NODE_ENV) {
-    case 'local':
-        APIURL = "http://localhost:3001/api/v1/";
-        SocialMediaURL = "http://localhost:3001/";
-        break;
-    case 'staging':
-        APIURL = "https://adpq-staging.hotbsoftware.com/api/v1/";
-        SocialMediaURL = "https://adpq-staging.hotbsoftware.com/";
-        break;
-    case 'production':
-        APIURL = "https://adpq.hotbsoftware.com/api/v1/";
-        SocialMediaURL = "https://adpq.hotbsoftware.com/";
-        break;
-    default:
-        APIURL = "https://adpq-staging.hotbsoftware.com/api/v1/";
-        SocialMediaURL = "https://adpq-staging.hotbsoftware.com/";
-        break;
-}
+// console.log(process.env.NODE_ENV)
+
+// switch (process.env.NODE_ENV) {
+//     case 'local':
+//         APIURL = "http://localhost:3001/api/v1/";
+//         SocialMediaURL = "http://localhost:3001/";
+//         break;
+//     case 'staging':
+//         APIURL = "https://adpq-staging.hotbsoftware.com/api/v1/";
+//         SocialMediaURL = "https://adpq-staging.hotbsoftware.com/";
+//         break;
+//     case 'production':
+//         APIURL = "https://adpq.hotbsoftware.com/api/v1/";
+//         SocialMediaURL = "https://adpq.hotbsoftware.com/";
+//         break;
+// }
