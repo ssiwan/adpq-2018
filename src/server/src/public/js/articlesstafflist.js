3824c182a423fa22ca7d24d2ef5adf9f2429b765
HOTB\raghu.jonnala <rjonnala@counselordirect.com>
2018-03-12 14:33:11 -0700

    RJ - article list view page for staff

--- src/server/src/public/js/articlesstafflist.js
+++ src/server/src/public/js/articlesstafflist.js
@@ -8,16 +8,23 @@ $(document).ready(function(){
 
     if(!isEmpty(role) && !isEmpty(token))
     {
-    var table;
-    var url = APIURL + "adminDashboardApproved";
- 
-    if (role === "admin") {
-        $("#adminprofile").attr("href","edit-profile-admin.html?userId="+ userid);
-    }
-    else{
-        $("#adminprofile").attr("href","edit-profile-staff.html?userId="+ userid); 
-    }
+            if (role === "admin") {
+                $("#adminsettingsbtn").show();
+            }
+            if (role === "admin") {
+                $("#adminprofile").attr("href","edit-profile-admin.html?userId="+ userid);
+            }
+            else{
+                $("#adminprofile").attr("href","edit-profile-staff.html?userId="+ userid); 
+            }
+            var table;
+
+    var url = APIURL + "staffArticles";
+
     Load();
+
+
+
     function Load() {
         table = $('#articles').DataTable({
             "bDestroy": true,
@@ -34,20 +41,19 @@ $(document).ready(function(){
                    //console.log(json);
                    var return_data = new Array();
                    for (let index = 0; index < json.data.length; index++) {
-                    var id = json.data[index].id;
-                    var title = json.data[index].title;
-                    var agency = json.data[index].agency;
-                    var summary = json.data[index].summary;
-                    var name = json.data[index].createdBy.name.first + "  " + json.data[index].createdBy.name.last;
-                    var createdAt = convertToLocalDate(json.data[index].createdAt);
+                        var id = json.data[index].id;
+                        var title = json.data[index].title;
+                        var agency = json.data[index].agency;
+                        var summary = json.data[index].summary;
+                        var name = json.data[index].createdBy.name.first + "  " + json.data[index].createdBy.name.last;
+                        var createdAt = convertToLocalDate(json.data[index].createdAt);
                         return_data[index] = {
-                            'articleinfo':"<div class='trending-admin-approved'><div class='trending-left-column'><div class='left-row-one'><div class='left-title'><a href=articles-details-admin-history.html?articleId="+id+">"+title+"</a></div><div class='left-column-tools'></div></div><div class='left-row-two'><div class='left-agency'>"+agency+"</div></div><div class='left-row-three'><div class='left-shortdesc'>"+summary+"</div></div><div class='left-row-four'><div class='left-publish-date'><div class='author'>"+name+"</div></div><div class='left-column-tools'><div class='left-most-pubdate'>"+createdAt+"</div></div></div></div></div>",
+                            'articleinfo':"<div class='trending-landing-articles'><div class='trending-left-column'><div class='left-row-one'><div class='left-title'><a href=articles-details-admin-history.html?articleId="+id+">"+title+"</a></div><div class='left-column-tools'></div></div><div class='left-row-two'><div class='left-agency'>"+agency+"</div></div><div class='left-row-three'><div class='left-shortdesc'>"+summary+"</div></div><div class='left-row-four'><div class='left-publish-date'><div class='author'>"+name+"</div></div><div class='left-column-tools'><div class='left-most-pubdate'>"+createdAt+"</div></div></div></div></div>",
                             'lastupdated': convertToLocalDate(json.data[index].createdAt),
                             'views': json.data[index].views,
                             'shares': json.data[index].shares,
                             'id': json.data[index].id
                         }
-                        
                    }
                    //console.log(return_data);
                    return return_data;
@@ -64,19 +70,18 @@ $(document).ready(function(){
                     { "data": "articleinfo", "autoWidth": true },
                     { "data": "lastupdated", "autoWidth": true },
                     { "data": "views", "autoWidth": true },
-                    { "data": "shares", "autoWidth": true },   
-                    { "data": "id", "autoWidth": true }    
+                    { "data": "shares", "autoWidth": true },
+                    { "data": "id", "autoWidth": true }     
             ]
         }); 
     }
     
 
+
     $("#logout").click(function() {
         sessionStorage.clear();
         window.location.href = "index.html";
     })
-
-    
     }
     else {
         window.location.href = "index.html";
